7af42722f02db7968ffd3cce0d6b96fa336acbf5
riko-well <riko.10390@mhs.amikomsolo.ac.id>
2025-02-20 17:30:34 +0700

    Add files via upload

---
+++ team/perkenalan.py
@@ -0,0 +1,11 @@
+def perkenalan(nama, nim, peran):
+    print("Halo! Perkenalkan, Nama saya", nama)
+    print("NIM :", nim)
+    print("Peran saya yaitu :", peran)
+
+if __name__ == "__main__":
+    nama = "Riko Ferdiansyah"
+    nim = "2213010390"
+    peran = "Mengerjakan bagian home, dashboard, halaman admin"
+
+    perkenalan(nama, nim, peran)
