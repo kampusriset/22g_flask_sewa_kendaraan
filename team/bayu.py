9d8b19422333e5bc54e68cc9f6235d1dae82b962
bytomo <bybyu.pc@gmail.com>
2025-02-20 17:35:19 +0700

    Add files via upload

---
+++ team/bayu.py
@@ -0,0 +1,11 @@
+def perkenalan(nama, nim, peran):
+    print("Halo! Perkenalkan, Nama saya", nama)
+    print("NIM :", nim)
+    print("Peran saya yaitu :", peran)
+
+if __name__ == "__main__":
+    nama = "Bayu"
+    nim = "2213010405"
+    peran = "Mengerjakan bagian CRUD"
+
+    perkenalan(nama, nim, peran)
