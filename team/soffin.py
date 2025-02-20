333153057c01aa724906821e22b8023fa7911ac3
soffin1-ta <soffinthoriq25@gmail.com>
2025-02-20 18:43:19 +0700

    Add files via upload

---
+++ team/soffin.py
@@ -0,0 +1,11 @@
+def perkenalan(nama, nim, peran):
+    print("halo perkenalkan nama saya ", nama)
+    print("NIM :", nim)
+    print("peran saya yaitu :", peran)
+
+if __name__ == "__main__":
+    nama = "soffin thoriq arfian"
+    nim = "2213010406"
+    peran = "mengerjakan tampilan pesan dan membantu dasboard"
+
+    perkenalan(nama, nim, peran)
