d0819e89ba04e9654430a9bbb81f8ca45f463d24
galihumb14 <jalih1234@gmail.com>
2025-02-20 23:19:06 +0700

    Add files via upload

---
+++ team/galih.py
@@ -0,0 +1,11 @@
+def perkenalan(nama, nim, peran):
+    print("Halo! Perkenalkan, Nama saya", nama)
+    print("NIM :", nim)
+    print("Peran saya yaitu :", peran)
+
+if __name__ == "__main__":
+    nama = "Galih"
+    nim = "2213010404"
+    peran = "Mengerjakan bagian Login, Signup, Ubah password"
+
+    perkenalan(nama, nim, peran)
